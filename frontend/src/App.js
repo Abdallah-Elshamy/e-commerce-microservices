0af31151f7823593a78fa4b3ebc445e0a95794c6
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-01-30 20:14:53 +0200

    Render products dynamically

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import data from "./data"
 
 function App() {
   const openMenu = () => {
@@ -33,51 +34,25 @@ function App() {
       <main className="main">
         <div className="content">
           <ul className="products">
-            <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/shirt.webp"
-                  alt="product"
-                />
-                <div className="product-name">
-                  <a href="product.html">Shirt</a>
+            {data.products.map((product) => (
+              <li>
+                <div className="product">
+                  <img
+                    className="product-image"
+                    src={product.image}
+                    alt="product"
+                  />
+                  <div className="product-name">
+                    <a href="product.html">{product.name}</a>
+                  </div>
+                  <div className="product-brand">{product.brand}</div>
+                  <div className="product-price">{product.price} $</div>
+                  <div className="product-rating">
+                    {product.rating} stars ({product.num_reviews} Reviews)
+                  </div>
                 </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">60$</div>
-                <div className="product-rating">4.5 stars (10 reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="images/shirt.webp"
-                  alt="product"
-                />
-                <div className="product-name">
-                  <a href="product.html">Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">60$</div>
-                <div className="product-rating">4.5 stars (10 reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="images/shirt.webp"
-                  alt="product"
-                />
-                <div className="product-name">
-                  <a href="product.html">Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">60$</div>
-                <div className="product-rating">4.5 stars (10 reviews)</div>
-              </div>
-            </li>
+              </li>
+            ))}
           </ul>
         </div>
       </main>
---
+++ frontend/src/data.js
@@ -0,0 +1,33 @@
+const data = {
+  products: [
+    {
+      name: "shirt",
+      image: "/images/shirt.webp",
+      category: "shirt",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      num_reviews: 10,
+    },
+    {
+      name: "shirt",
+      image: "/images/shirt.webp",
+      category: "shirt",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      num_reviews: 10,
+    },
+    {
+      name: "shirt",
+      image: "/images/shirt.webp",
+      category: "shirt",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      num_reviews: 10,
+    },
+  ],
+};
+
+export default data
