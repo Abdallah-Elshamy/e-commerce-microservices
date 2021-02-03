768e1567beade910def318ca26f81abe881317a1
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-02-03 09:59:46 +0200

    Create a dummy server express

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,21 @@
-import  express  from "express"
+import express from "express";
 
-const app = express()
+const app = express();
+
+app.get("/api/products", (req, res) => {
+   console.log("here");
+  res.send({
+    _id: "1",
+    name: "shirt",
+    image: "/images/shirt.webp",
+    category: "shirt",
+    price: 60,
+    brand: "Nike",
+    rating: 4.5,
+    num_reviews: 10,
+  });
+});
 
 app.listen(5000, () => {
-    console.log("server started at http://localhost:5000")
-})
+  console.log("server started at http://localhost:5000");
+});
