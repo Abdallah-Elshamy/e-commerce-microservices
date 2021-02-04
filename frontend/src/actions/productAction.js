8a15e9e0dcf4eea23e00821fbd8091c5a1f7dae0
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-02-04 12:40:08 +0200

    Add action to get product details

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -4,6 +4,9 @@ import {
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
 } from "../constants/productConstant";
 
 const listProducts = () => async (dispatch) => {
@@ -16,4 +19,14 @@ const listProducts = () => async (dispatch) => {
   }
 };
 
-export { listProducts };
+const detailsProduct = (productId) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId })
+    const { data } = await axios.get(apiEndpoint + `products/${productId}`);
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (err) {
+    dispatch({ type: PRODUCT_DETAILS_FAIL, payload: err.message });
+  }
+}
+
+export { listProducts, detailsProduct };
