2b14a246b08f2be152b1a203aa683c4e88b4e7be
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-02-10 20:23:51 +0200

    add addToCart action

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { apiEndpoint } from "../config";
+import { CART_ADD_ITEM } from "../constants/cartConstant";
+
+const addToCart = (productId, qty) => async (dispatch) => {
+  try {
+    const { data } = await axios.get(apiEndpoint + `products/${productId}`);
+    dispatch({
+      type: CART_ADD_ITEM,
+      payload: {
+        product: data._id,
+        name: data.name,
+        image: data.image,
+        price: data.price,
+        count: data.countInStock,
+        qty,
+      },
+    });
+  } catch (error) {}
+};
+
+export { addToCart }
