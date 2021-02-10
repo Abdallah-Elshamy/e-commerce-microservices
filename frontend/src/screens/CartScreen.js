7deb62249bf5cc38dc0cdeb2e4cba2bf3b4cc828
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-02-10 20:26:19 +0200

    Add cart screen

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../actions/cartActions";
+function CartScreen(props) {
+  const cart = useSelector((state) => state.cart);
+
+  const { cartItems } = cart;
+
+  const productId = props.match.params.id;
+  const qty = props.location.search
+    ? Number(props.location.search.split("=")[1])
+    : 1;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
+  return (
+    <div className="cart">
+      <div className="cart-list">
+        <ul className="cart-list-container">
+          <li>
+            <h3>Shopping Cart</h3>
+            <div>Price</div>
+          </li>
+          {cartItems.length === 0 ? (
+            <div> Cart is </div>
+          ) : (
+            cartItems.map((item) => (
+              <div>
+                <img src={item.image} alt="product" />
+                <div className="cart-name">
+                  <div>{item.name}</div>
+                  <div>
+                    Qty:
+                    <select>
+                    {[...Array(item.countInStock).keys()].map((x) => (
+                    <option key={x + 1} value={x + 1}>
+                      {x + 1}
+                    </option>
+                  ))}
+                    </select>
+                  </div>
+                </div>
+                <div>{item.price}</div>
+              </div>
+            ))
+          )}
+        </ul>
+      </div>
+      <div className="cart-action">
+        <h3>
+          Subtotal ({cartItems.reduce((a, c) => a + c.qty, 0)} items) : ${" "}
+          {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+        </h3>
+        <button className="button primary" disabled={cartItems === 0}>
+          Proceed to Checkout
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CartScreen;
