49057a2c764f3b5e5a5ab298cb0dddb1e317dbe3
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-02-12 11:48:52 +0200

    Use cookies to save items in cart

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import Cookie from "js-cookie";
 import { apiEndpoint } from "../config";
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstant";
 
-const addToCart = (productId, qty) => async (dispatch) => {
+const addToCart = (productId, qty) => async (dispatch, getState) => {
   try {
     const { data } = await axios.get(apiEndpoint + `products/${productId}`);
     dispatch({
@@ -16,15 +17,23 @@ const addToCart = (productId, qty) => async (dispatch) => {
         qty,
       },
     });
+    const {
+      cart: { cartItems },
+    } = getState();
+    Cookie.set("cartItems", JSON.stringify(cartItems));
   } catch (error) {}
 };
 
-const removeFromCart = (productId) => (dispatch) => {
+const removeFromCart = (productId) => async (dispatch, getState) => {
   try {
     dispatch({
       type: CART_REMOVE_ITEM,
       payload: productId,
     });
+    const {
+      cart: { cartItems },
+    } = getState();
+    Cookie.set("cartItems", JSON.stringify(cartItems));
   } catch (error) {}
 };
 
