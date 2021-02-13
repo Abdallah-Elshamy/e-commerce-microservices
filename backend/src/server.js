764c86352f6044d5dc043a264b4d5e3fc7878fac
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-02-13 10:30:12 +0200

    Use the new Server Discover and Monitoring engine for MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,8 @@ import mongoose from "mongoose"
 import config from "./config.js"
 
 mongoose.connect(config.MONGODB_URL, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
 
 const app = express();
