be7cece0b3400786a1401277c683db3b0e0816ca
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-02-13 22:47:30 +0200

    Add an action to register

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -5,12 +5,18 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
 } from "../constants/userConstants";
 
 const login = (email, password) => async (dispatch) => {
   dispatch({ type: USER_LOGIN_REQUEST, payload: { email, password } });
   try {
-    const { data } = await Axios.post(apiEndpoint + "/users/login", { email, password });
+    const { data } = await Axios.post(apiEndpoint + "/users/login", {
+      email,
+      password,
+    });
     dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
     Cookie.set("userInfo", JSON.stringify(data));
   } catch (error) {
@@ -18,4 +24,19 @@ const login = (email, password) => async (dispatch) => {
   }
 };
 
-export { login };
+const register = (name, email, password) => async (dispatch) => {
+  dispatch({ type: USER_REGISTER_REQUEST, payload: { name, email, password } });
+  try {
+    const { data } = await Axios.post(apiEndpoint + "/users/register", {
+      name,
+      email,
+      password,
+    });
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+    Cookie.set("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({ type: USER_REGISTER_FAIL, payload: error.message });
+  }
+};
+
+export { login, register };
