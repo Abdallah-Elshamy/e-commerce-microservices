f091ba63ae94b5c54285b257342592067c80a6df
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-02-13 13:29:27 +0200

    Use bycrypt in comparing passwords

--- backend/src/controllers/v0/users/routes/user.router.js
+++ backend/src/controllers/v0/users/routes/user.router.js
@@ -1,26 +1,31 @@
 import express from "express";
 import User from "../models/user.js";
-import { generatePassword, getToken } from "../utils.js";
+import { generatePassword, comparePasswords, getToken } from "../utils.js";
 
 const router = express.Router();
 
 router.post("/login", async (req, res) => {
   const user = await User.findOne({
     email: req.body.email,
-    password: await generatePassword(req.body.password),
   });
 
-  if (user) {
-    res.send({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token: getToken(user),
-    });
-  } else {
-    res.status.status(401).send({ msg: "Invalid Email or Password." });
+  if (!user) {
+    return res.status(401).send({ msg: "Invalid Email." });
   }
+
+  const validAuth = await comparePasswords(req.body.password, user.password);
+
+  if (!validAuth) {
+    return res.status(401).send({ msg: "Invalid Password." });
+  }
+
+  return res.send({
+    _id: user.id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+    token: getToken(user),
+  });
 });
 
 export const UserRouter = router;
