c277123c2de50be91a8f3afc403dd03b83591292
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-02-14 00:00:10 +0200

    Add middleware to check if the user is authenticated and if the user is admin

--- backend/src/controllers/v0/users/utils.js
+++ backend/src/controllers/v0/users/utils.js
@@ -25,3 +25,34 @@ export async function getToken(user) {
     }
   );
 }
+
+export function isAuth(req, res, next) {
+  if (!req.headers || !req.headers.authorization) {
+    return res.status(401).send({ message: "No authorization headers." });
+  }
+
+  const token_bearer = req.headers.authorization.split(" ");
+  if (token_bearer.length != 2) {
+    return res.status(401).send({ message: "Malformed token." });
+  }
+
+  const token = token_bearer[1];
+
+  return jwt.verify(token, config.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(500)
+        .send({ auth: false, message: "Failed to authenticate." });
+    }
+    req.user = decoded;
+    return next();
+  });
+}
+
+export function isAdmin(req, res, next) {
+  if (req.user && req.user.isAdmin) {
+    return next();
+  } else {
+    return res.status(401).send({ message: "Failed to authenticate Admin." });
+  }
+}
