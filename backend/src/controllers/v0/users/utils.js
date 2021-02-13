985fdde76162bae938071f504d35c54077a9c7ed
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-02-13 13:28:45 +0200

    Add a util function to compare passwords and put the right data in the JWT tokens

--- backend/src/controllers/v0/users/utils.js
+++ backend/src/controllers/v0/users/utils.js
@@ -1,14 +1,27 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-import config from "../../../config.js"
+import config from "../../../config.js";
 
 export async function generatePassword(plainTextPassword) {
-    return bcrypt.hash(plainTextPassword, 10);
+  return bcrypt.hash(plainTextPassword, 10);
+}
+
+export async function comparePasswords(plainTextPassword, hash) {
+  return bcrypt.compare(plainTextPassword, hash);
 }
 
 export async function getToken(user) {
-    return jwt.sign(user, config.JWT_SECRET, {
-        expiresIn: "48h"
-    });
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    config.JWT_SECRET,
+    {
+      expiresIn: "48h",
+    }
+  );
 }
