e65e9f96ecc093b9a3f2fde549abdd83f20c71d9
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-02-14 11:47:24 +0200

    Get all products from database instead of the stub data

--- backend/src/controllers/v0/products/routes/product.router.js
+++ backend/src/controllers/v0/products/routes/product.router.js
@@ -1,22 +1,18 @@
 import express from "express";
 
+import Product from "../models/product.js";
+
+const PAGE_SIZE = 10;
+
 const router = express.Router();
 
 // Get all products
-router.get("/", (req, res) => {
-  res.send([
-    {
-      _id: "1",
-      name: "shirt",
-      image: "/images/shirt.webp",
-      category: "shirt",
-      price: 60,
-      brand: "Nike",
-      rating: 4.5,
-      num_reviews: 10,
-      countInStock:6,
-    },
-  ]);
+router.get("/", async (req, res) => {
+  products = await Product.find({}, null, {
+    skip: ((req.query.page || 1) - 1) * PAGE_SIZE,
+    limit: PAGE_SIZE,
+  }).exec();
+  res.send(products);
 });
 
 // Get a product detail
@@ -30,7 +26,7 @@ router.get("/:id", (req, res) => {
     brand: "Nike",
     rating: 4.5,
     num_reviews: 10,
-    countInStock:6,
+    countInStock: 6,
   });
 });
 
