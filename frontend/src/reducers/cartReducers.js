89c9349c359fc9956e50a890b639ba10f74dd8b8
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-02-14 22:58:18 +0200

    Add a reducer to save payment method in the state

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -2,6 +2,7 @@ import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
   CART_SAVE_SHIPPING,
+  CART_SAVE_PAYMENT,
 } from "../constants/cartConstant";
 
 function cartReducer(state = { cartItems: [] }, action) {
@@ -26,6 +27,8 @@ function cartReducer(state = { cartItems: [] }, action) {
       };
     case CART_SAVE_SHIPPING:
       return { ...state, shipping: action.payload };
+    case CART_SAVE_PAYMENT:
+      return { ...state, payment: action.payload };
     default:
       return state;
   }
