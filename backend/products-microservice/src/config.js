b14b03e13b42930fce3a2d8a59adc151a649c6b9
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-02-15 19:38:50 +0200

    Generate signed urls to get and put items into S3 bucket

--- backend/products-microservice/src/config.js
+++ backend/products-microservice/src/config.js
@@ -1,4 +1,7 @@
 export default {
   MONGODB_URL: process.env.MONGODB_URL,
   JWT_SECRET: process.env.JWT_SECRET,
+  aws_region: process.env.AWS_REGION,
+  aws_profile: process.env.AWS_PROFILE,
+  aws_media_bucket: process.env.AWS_BUCKET,
 };
