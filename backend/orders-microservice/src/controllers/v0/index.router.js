82a22dd87fa3ad8fcb1cd93db6b3ef536de155c1
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-02-15 13:30:42 +0200

    Remove unnecessary code from orders-microservice

--- backend/orders-microservice/src/controllers/v0/index.router.js
+++ backend/orders-microservice/src/controllers/v0/index.router.js
@@ -1,12 +1,8 @@
 import express from "express";
-import { ProductRouter } from "./products/routes/product.router.js";
-import { UserRouter } from "./users/routes/user.router.js";
 import { OrderRouter } from "./orders/routes/order.router.js";
 
 const router = express.Router();
 
-router.use("/products", ProductRouter);
-router.use("/users", UserRouter);
 router.use("/orders", OrderRouter);
 
 router.get("/", async (req, res) => {
